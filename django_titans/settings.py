489542f62aac724a02936928311db8d7d2c9c8bd
Yazan El Masri <yazan_elmasri@hotmail.com>
2024-06-27 11:38:48 +0000

    first deployment

--- django_titans/settings.py
+++ django_titans/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
+import os
 from pathlib import Path
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +32,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-'8000-yazanelmasr-djangotitan-fb5pdf38tv7.ws-eu114.gitpod.io'
+'8000-yazanelmasr-djangotitan-fb5pdf38tv7.ws-eu114.gitpod.io',
+ '.herokuapp.com'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.8000-yazanelmasr-djangotitan-fb5pdf38tv7.ws-eu114.gitpod.io",
+    "https://*.herokuapp.com"
 ]
 
 
@@ -77,11 +88,16 @@ WSGI_APPLICATION = 'django_titans.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default':
+dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
