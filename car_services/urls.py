8f05902389bdbb5c749dd6ceeee986f8e57f295d
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-06-27 10:00:26 +0100

    add template files for accounts, appointments, vehicles and services

--- car_services/urls.py
+++ car_services/urls.py
@@ -1,9 +1,19 @@
 # urls.py
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
     path('appointments/', views.appointment_list, name='appointment_list'),
     path('appointment/create/', views.create_appointment, name='create_appointment'),
     path('appointment/<int:pk>/', views.appointment_detail, name='appointment_detail'),
