a9204f364dfdceaf7884d2cce69bf6918b233726
IGWE, Chibueze C. (Data Analyst & Software Engineer Enthusiast) <scientistigwe@gmail.com>
2024-06-28 11:35:40 +0000

    add staticfiles

--- django_titans/settings.py
+++ django_titans/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This directory should include additional static file locations (if any)
@@ -159,5 +159,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 LOGIN_REDIRECT_URL = 'appointment_list'
 LOGOUT_REDIRECT_URL = ''
-
-heroku config:set DISABLE_COLLECTSTATIC=1
