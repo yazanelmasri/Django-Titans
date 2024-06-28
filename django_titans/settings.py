a6ec270063903a7b1da5727dd492dcac4a0c81e1
IGWE, Chibueze C. (Data Analyst & Software Engineer Enthusiast) <scientistigwe@gmail.com>
2024-06-28 10:41:10 +0000

    add staticfiles

--- django_titans/settings.py
+++ django_titans/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'car_services/static/car_services'
+STATIC_URL = '/car_services/static/car_services'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/car_services')
 
 # Default primary key field type
