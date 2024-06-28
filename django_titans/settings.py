4c723c99a5d3655a7d12458a0ca3ad0c6c82d46c
IGWE, Chibueze C. (Data Analyst & Software Engineer Enthusiast) <scientistigwe@gmail.com>
2024-06-28 10:27:29 +0000

    add staticfiles

--- django_titans/settings.py
+++ django_titans/settings.py
@@ -19,8 +19,8 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
